c7b5e7655fedcbcdddbf501748751763ece798a8
Som Mondal <sommondal5872@gmail.com>
2025-10-05 23:27:27 +0530

    5/10/2025

--- backtracking/Rat in a Maze/Solution.java
+++ backtracking/Rat in a Maze/Solution.java
@@ -43,7 +43,6 @@ class Solution {
 
     public static void main(String [] args){
         int [][] a = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}};
-        System.out.println(ratInMaze(a));
         
     }
 }
